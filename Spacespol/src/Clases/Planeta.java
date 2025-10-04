e2c7a0b35e1a43472d1beb631206bc6c00cb03e9
FiorellaQuijano <fioanqui@espol.edu.ec>
2025-10-04 15:00:39 -0500

    creacion de metodos clase Planeta

--- Spacespol/src/Clases/Planeta.java
+++ Spacespol/src/Clases/Planeta.java
@@ -14,9 +14,47 @@ public class Planeta {
     String nombre;
     double distancia; // Para calcular tiempo de viaje
     List<Objeto> muestras;
+    private boolean explorado;
 
-    void recolectarMuestra(Astronauta astronauta, Objeto muestra){
-        
+    public Planeta(String nombre, double distancia, List<Objeto> muestras, boolean explorado) {
+        this.nombre = nombre;
+        this.distancia = distancia;
+        this.muestras = muestras;
+        this.explorado = explorado;
+    }
+    
+    public void agregarMuestra(Objeto muestra) {
+        muestras.add(muestra);
+    }
+    
+    public boolean recolectarMuestraPlaneta(Astronauta astronauta, Objeto muestra) {
+        if (muestras.contains(muestra)) {
+            astronauta.recolectarMuestras(this, muestra);
+            muestras.remove(muestra);
+            System.out.println("✅ " + astronauta.getNombre() + " ha recolectado: " + muestra.getNombre());
+            return true;
+        } else {
+            System.out.println("⚠️ La muestra " + muestra.getNombre() + " no está disponible en " + nombre);
+            return false;
+        }
+    }
+ 
+    public String getNombre() {
+        return nombre;
     }
 
+    public double getDistancia() {
+        return distancia;
+    }
+
+    public List<Objeto> getMuestras() {
+        return muestras;
+    }
+
+    public boolean isExplorado() {
+        return explorado;
+    }
+
+    
+   
 }
