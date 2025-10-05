334f220fe7bb436c398b434d0e3bbc57b9261732
angie329 <aalfonso@espol.edu.ec>
2025-10-05 15:02:36 -0500

    Modificacion de varias vistas

--- Spacespol/src/Controladores/SeleccionModuController.java
+++ Spacespol/src/Controladores/SeleccionModuController.java
@@ -15,7 +15,10 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
@@ -28,6 +31,21 @@ public class SeleccionModuController implements Initializable {
 
     @FXML
     private Button retrocederBoton;
+    
+    @FXML
+    private Button siguienteBoton;
+    
+    @FXML
+    private Circle circulo1;
+    
+    @FXML
+    private Circle circulo2;
+    
+    @FXML
+    private Circle circulo3;
+    
+    @FXML
+    private Circle circulo4;
     
     @FXML
     private Button logisBoton;
@@ -64,7 +82,30 @@ public class SeleccionModuController implements Initializable {
     } catch (IOException ex) {
         ex.printStackTrace();
     }
+    }
+    
+    @FXML
+    public void siguiente(){
+        try {
+        Parent root = FXMLLoader.load(getClass().getResource("/Vistas/EstacionEspacial.fxml"));
+        Stage stage = (Stage) retrocederBoton.getScene().getWindow(); // obtener ventana actual 
+        stage.setScene(new Scene(root));
+        stage.show();
+    } catch (IOException ex) {
+        ex.printStackTrace();
+    }
+    }
         
+    
+    @FXML
+    private void toggleCircle(MouseEvent event) {
+        Circle circle = (Circle) event.getSource();
+
+        if (circle.getFill().equals(Color.WHITE)) {
+            circle.setFill(Color.LIMEGREEN); // cambia a verde
+        } else {
+            circle.setFill(Color.WHITE); // vuelve a blanco
+        }
     }
     }
     
